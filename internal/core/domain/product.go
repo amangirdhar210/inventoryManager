f017810205720c73d2580135c8cf0377840bec52
Aman <amangirdhar2004@gmail.com>
2025-09-15 04:38:42 +0000

    removed IsLowOnStock from product.go (no functions in product.go now hence no testing req)

--- internal/core/domain/product.go
+++ internal/core/domain/product.go
@@ -1,9 +1,6 @@
 package domain
 
 import (
-	"errors"
-
-	"github.com/amangirdhar210/inventory-manager/config"
 	"github.com/google/uuid"
 )
 
@@ -14,36 +11,12 @@ type Product struct {
 	Quantity int
 }
 
-func (product *Product) Validate() error {
-	if product.Name == "" {
-		return errors.New("product name cannot be empty")
-	} else if !isGreaterThanZero(product.Price) {
-		return errors.New("product price must be greater than zero")
-	} else if product.Quantity < 0 {
-		return errors.New("product quantity cannot be negative")
-	} else {
-		return nil
-	}
-}
-
-func NewProduct(name string, price float64, quantity int) (*Product, error) {
+func NewProduct(name string, price float64, quantity int) *Product {
 	product := &Product{
 		Id:       uuid.New().String(),
 		Name:     name,
 		Price:    price,
 		Quantity: quantity,
 	}
-
-	if err := product.Validate(); err != nil {
-		return nil, err
-	}
-	return product, nil
-}
-
-func isGreaterThanZero[T int | float64](AmountOrQuantity T) bool {
-	return AmountOrQuantity > 0
-}
-
-func (product *Product) IsLowOnStock() bool {
-	return product.Quantity < config.ThresholdAlertQty
+	return product
 }
