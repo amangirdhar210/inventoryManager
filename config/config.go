dbe0566525db616b7446f9a59f2507fc505a6873
Aman <amangirdhar2004@gmail.com>
2025-09-25 04:10:54 +0000

    edit

--- config/config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-const ThresholdAlertQty int = 10
-
-const JWTSecretKey string = "YOUR_JWT_SECRET_KEYHERE"
-const AdminPassword string = "YOUR_ADMIN_PASSWORD"
-const AdminEmail string = "YOUR_ADMIN_EMAIL"
